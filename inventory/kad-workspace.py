63b5fd24355117c9f8d74c349d6ef93b0d2c57f2
陆勇思 <luyongsi@ruijie.com.cn>
2021-02-07 15:40:07 +0800

    运维平台1.0版本

--- inventory/kad-workspace.py
+++ inventory/kad-workspace.py
@@ -239,7 +239,20 @@ def parse_host_data(workspace_dir):
 
     parse_sourcedata_config(result)
 
+    parse_eoms_config(result)
+
     return result
+
+# 处理SourceData配置参数
+def parse_eoms_config(host_data):
+    group_all_vars = host_data["groups"]["all"]["vars"]
+
+    eoms_hosts =  group_all_vars["EOMS"]["EOMS_STORAGE_HOST"] if "EOMS" in group_all_vars else []
+    for ip in eoms_hosts:
+        if not is_IP(ip):
+            raise Exception(ip + u"不是有效的IP地址")
+    host_data["groups"]["influxdb"] = eoms_hosts
+
 
 # 处理SourceData配置参数
 def parse_sourcedata_config(host_data):
