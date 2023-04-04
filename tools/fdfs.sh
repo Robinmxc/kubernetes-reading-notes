691967ac883bb97837a845b48f76d2a9c13bad0e
bichonghai <Bch8878255>
2023-04-04 11:33:10 +0800

    fdfs修改提交

---
+++ tools/fdfs.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+osname=(`uname -r`)
+rm -rf /opt/kad/down/fdfs/rpms/${osname}
+mkdir -p /opt/kad/down/fdfs/rpms/${osname}
+cd /opt/kad/down/fdfs/rpms/${osname}
+result=$(echo $osname | grep ".el7.x86_64")
+function centos7(){
+	echo "centos7 call"
+    rpms="autoconf automake cpp gcc gcc-c++  glibc-devel  glibc-headers  keepalived  kernel-headers  keyutils-libs-devel  krb5-devel \
+          libcom_err-devel libkadm5 libmpc libselinux-devel libsepol-devel libstdc++-devel \
+             libtool  libverto-devel  lm_sensors-libs m4  mpfr  net-snmp-agent-libs \
+               net-snmp-libs    openssl-devel   pcre-devel  perl-Data-Dumper   perl-Test-Harness  perl-Thread-Queue  zlib-devel "  
+	yum install -y $rpms --downloadonly  --downloaddir=./
+}  
+function AnolisOS(){
+	echo "AnolisOS call"
+    rpms="autoconf automake cpp gcc gcc-c++  glibc-devel  glibc-headers  keepalived  kernel-headers  keyutils-libs-devel  krb5-devel \
+          libcom_err-devel libkadm5 libmpc libselinux-devel libsepol-devel libstdc++-devel \
+             libtool  libverto-devel  lm_sensors-libs m4  mpfr  net-snmp-agent-libs \
+               net-snmp-libs    openssl-devel   pcre-devel  perl-Data-Dumper   perl-Test-Harness  perl-Thread-Queue  zlib-devel "
+      
+	yum install -y $rpms --downloadonly  --downloaddir=./
+}  
+function openEulerOs(){
+	echo "openEulerOs call"
+    rpms="autoconf automake cpp gcc gcc-c++  glibc-devel  glibc-headers  keepalived  kernel-headers  keyutils-libs-devel  krb5-devel \
+          libcom_err-devel libkadm5 libmpc libselinux-devel libsepol-devel libstdc++-devel \
+             libtool  libverto-devel  lm_sensors-libs m4  mpfr  net-snmp-agent-libs \
+               net-snmp-libs    openssl-devel   pcre-devel  perl-Data-Dumper   perl-Test-Harness  perl-Thread-Queue  zlib-devel "
+      
+	yum install -y $rpms --downloadonly  --downloaddir=./ 
+}    
+result=$(echo $osname | grep ".oe2203.x86_64")
+if	[[ "$result" != "" ]] ;then
+	openEulerOs
+fi
+result=$(echo $osname | grep ".an8.x86_64")
+if	[[ "$result" != "" ]];then
+	AnolisOS
+fi
+result=$(echo $osname | grep ".el7.x86_64")
+if	[[ "$result" != "" ]];then
+	centos7
+fi
+cd /opt/kad/down/fdfs/rpms/
+rm -rf fastdfs.tar.gz
+tar  -zcvf   fastdfs.tar.gz *
+rm -rf /opt/kad/down/fdfs/fastdfs.tar.gz
+cp fastdfs.tar.gz /opt/kad/down/fdfs/
