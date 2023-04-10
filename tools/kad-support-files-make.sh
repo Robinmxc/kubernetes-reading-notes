cfca187cddc28a38458ff3a39a76a129ebf783ac
bichonghai <Bch8878255>
2023-04-10 14:30:02 +0800

    更新kad依赖2.9.1

--- tools/kad-support-files-make.sh
+++ tools/kad-support-files-make.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -o errexit
 set -o pipefail
-rm -rf kad-support-files-2.9.0
-mkdir  kad-support-files-2.9.0
-mkdir  kad-support-files-2.9.0/bin
-mkdir  kad-support-files-2.9.0/down 
-mkdir  kad-support-files-2.9.0/down/rpms   
+rm -rf kad-support-files-2.9.1
+mkdir  kad-support-files-2.9.1
+mkdir  kad-support-files-2.9.1/bin
+mkdir  kad-support-files-2.9.1/down 
+mkdir  kad-support-files-2.9.1/down/rpms   
 downFiles=( addon-resizer-1.8.3.tar  calico_v3.4.3.tar  dashboard_v1.10.1.tar  heapster_v1.5.4.tar  metrics-server_v0.3.1.tar  mongo-4.0.24.tar       rocketmq-4.3.0.tar  \
 	busybox-1.30.1.tar         mgob-1.0.tar               postgres-13.6.tar                        \
 	coredns-v1.8.6.tar  etcd-3.5.1-0.tar  flannel-cni-plugin-v1.1.2.tar  flannel-v0.20.2.tar  kube-apiserver-v1.23.8.tar     \
@@ -13,15 +13,15 @@ downFiles=( addon-resizer-1.8.3.tar  calico_v3.4.3.tar  dashboard_v1.10.1.tar  h
 	sourceid-kad-dlmu.r1.3.zip)
 for var in ${downFiles[@]};
 do
-	cp -r /opt/kad/down/${var} ./kad-support-files-2.9.0/down
+	cp -r /opt/kad/down/${var} ./kad-support-files-2.9.1/down
 done
 rpmsFile=(5.10.0-60.18.0.50.oe2203.x86_64 4.19.91-26.an8.x86_64 3.10.0-957.el7.x86_64)
 for var in ${rpmsFile[@]};
 do
-	mkdir ./kad-support-files-2.9.0/down/rpms/${var}
-	cp -r  /opt/kad/down/rpms/${var} ./kad-support-files-2.9.0/down/rpms/
+	mkdir ./kad-support-files-2.9.1/down/rpms/${var}
+	cp -r  /opt/kad/down/rpms/${var} ./kad-support-files-2.9.1/down/rpms/
 done
-cd    kad-support-files-2.9.0
-tar  -zcvf   kad-support-files-2.9.0.tar.gz bin down
+cd    kad-support-files-2.9.1
+tar  -zcvf   kad-support-files-2.9.1.tar.gz bin down
 cd ..
-mv kad-support-files-2.9.0/kad-support-files-2.9.0.tar.gz .
+mv kad-support-files-2.9.1/kad-support-files-2.9.1.tar.gz .
