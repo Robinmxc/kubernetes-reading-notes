71d10e97ce5f0b307c4bc7cfe4eda16779028e33
bichonghai <Bch8878255>
2023-04-13 16:49:09 +0800

    解决龙溪最小版本没有安装python3的问题

--- kad-play.sh
+++ kad-play.sh
@@ -4,5 +4,14 @@ result=$(echo $osname | grep ".el7.x86_64")
 if	[[ "$result" != "" ]];then
  	ansible-playbook -i inventory/ -k $* 
 else
-    ansible-playbook -i inventory/ -k $* -e 'ansible_python_interpreter=/usr/bin/python3'
+    result_an8=$(echo $osname | grep ".an8.x86_64")
+    echo -n "SSH password: " 
+    read  ssh_password  
+    if	[[ "$result_an8" != "" ]];then
+   	 chmod +777 ./tools/*.py
+   	 set -o errexit
+    	./tools/env_process.py ${ssh_password}
+    	set +o errexit 
+    fi	
+    sshpass -p ${ssh_password}  ansible-playbook -i inventory/ -k $* -e 'ansible_python_interpreter=/usr/bin/python3'
 fi
