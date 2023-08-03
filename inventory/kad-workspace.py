cd3182bcdee92a66a63a298140aa9d106bc9cdc7
bichonghai <Bch8878255>
2023-08-03 13:25:22 +0800

    解决ip判断key不存在问题

--- inventory/kad-workspace.py
+++ inventory/kad-workspace.py
@@ -383,7 +383,7 @@ def parse_eoms_config(host_data):
         host_data["groups"]["influxdb"] = eoms_hosts
         host_data["groups"]["eoms"] = eoms_hosts
         host_vars = host_data["host_vars"]
-        if not host_vars[ip]:
+        if  not ip in host_vars:
           host_vars[ip]={}
 
 # 处理SourceData配置参数
