3d3d9e3544165041e5d05c4fcf6631a6bdf9ae5d
bichonghai <Bch8878255>
2023-11-24 13:55:50 +0800

    支持非root用户修复

--- tools/env_process.py
+++ tools/env_process.py
@@ -36,12 +36,12 @@ def python3_rpm(ip):
         version_command_output = subprocess.check_output(version_command, shell=True).decode("utf-8")
       except Exception :
         print("远程安装python3:服务器IP="+ip+"")
-        del_command="sshpass -p "+ ssh_password+  " ssh -p "+ssh_port+" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "+ssh_user+"@"+ip+" \""+sudo+" rm -rf /usr/bin/python39;rm -rf /usr/bin/pip3;rm -rf /usr/local/python3;mkdir -p /usr/local/python3 > /dev/null 2>&1;\" "
+        del_command="sshpass -p "+ ssh_password+  " ssh -p "+ssh_port+" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "+ssh_user+"@"+ip+" \""+sudo+" rm -rf /usr/bin/python39;"+sudo+"rm -rf /usr/bin/pip3;"+sudo+"rm -rf /usr/local/python3;"+sudo+"mkdir -p /usr/local/python3 > /dev/null 2>&1;\" "
         del_command_output= subprocess.check_output(del_command, shell=True)   
         copy_command="sshpass -p "+ ssh_password+ " scp  -r -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null /opt/kad/down/rpms/"+output_uname+"/python39/* "+ssh_user+"@"+ip+":/tmp/ > /dev/null 2>&1"
         copy_command_output = subprocess.check_output(copy_command, shell=True)   
         print("远程拷贝文件"+copy_command)
-        install_command="sshpass -p "+ ssh_password+  " ssh -p "+ssh_port+" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "+ssh_user+"@"+ip+" \""+sudo+"tar -xvf /tmp/python3.tar -C /usr/local/python3;cp /usr/local/python3/bin/python3.9 /usr/bin/python39;	cp /usr/local/python3/bin/pip3 /usr/bin/pip3;\" "
+        install_command="sshpass -p "+ ssh_password+  " ssh -p "+ssh_port+" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "+ssh_user+"@"+ip+" \""+sudo+"tar -xvf /tmp/python3.tar -C /usr/local/python3;"+sudo+"cp /usr/local/python3/bin/python3.9 /usr/bin/python39;	"+sudo+"cp /usr/local/python3/bin/pip3 /usr/bin/pip3;\" "
         install_command_output = subprocess.check_output(install_command, shell=True)   
         print("远程安装")   
   except Exception :
