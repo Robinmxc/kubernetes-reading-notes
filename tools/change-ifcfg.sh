c17f739eacbd44e20b06383555c0d2499c20d2b5
tangchuanbo <tangchuanbo@ruijie.com>
2024-01-25 10:12:37 +0800

    fix: 修改网卡配置

--- tools/change-ifcfg.sh
+++ tools/change-ifcfg.sh
@@ -4,3 +4,4 @@
 ifMark=`ip a|grep $2/ |awk '{print $NF}'`
 ifCfg=/etc/sysconfig/network-scripts/ifcfg-${ifMark}
 sed -i "s/IPADDR=$2/IPADDR=$1/g" ${ifCfg}
+sed -i 's/IPADDR="$2"/IPADDR="$1"/g' ${ifCfg}
